9e7d554b774f66a848126ae7c3fa35ea4f59d394
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-10-23 17:47:29 -0600

    feat: implement health check endpoint and set up HTTP server with API routes for MongoDB operations

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"mongo-manager/mongo"
-	"mongo-manager/types"
-
-	"go.mongodb.org/mongo-driver/v2/bson"
+	v1 "mongo-manager/api/v1"
+	"mongo-manager/auth"
+	"net/http"
+	"time"
 )
 
 func main() {
-	docs, err := mongo.GetAll(types.Request{
-		Database:   "general",
-		Collection: "nucleus",
-		Filter:     bson.D{},
-	})
 
-	if err != nil {
-		log.Fatalf("Error getting documents: %v", err)
+	http.HandleFunc("/health", healthCheck)
+
+	// V1 API
+
+	http.Handle("/v1/get-all", auth.VerifyingMiddleware(http.HandlerFunc(v1.GetAll)))
+	http.Handle("/v1/get-one", auth.VerifyingMiddleware(http.HandlerFunc(v1.GetOne)))
+	http.Handle("/v1/insert-one", auth.VerifyingMiddleware(http.HandlerFunc(v1.InsertOne)))
+	http.Handle("/v1/insert-many", auth.VerifyingMiddleware(http.HandlerFunc(v1.InsertMany)))
+	http.Handle("/v1/update-one", auth.VerifyingMiddleware(http.HandlerFunc(v1.UpdateOne)))
+	http.Handle("/v1/update-many", auth.VerifyingMiddleware(http.HandlerFunc(v1.UpdateMany)))
+	http.Handle("/v1/delete-one", auth.VerifyingMiddleware(http.HandlerFunc(v1.DeleteOne)))
+	http.Handle("/v1/delete-many", auth.VerifyingMiddleware(http.HandlerFunc(v1.DeleteMany)))
+
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+		Handler:      http.DefaultServeMux,
 	}
-	fmt.Println(docs)
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
